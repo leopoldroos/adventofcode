86c29f70468173a5fad8311c6a7767c3e9fc0cb2
Leopold Roos <leopold@leopold.se>
2017-12-03 00:18:43 +0100

    Still some startup-fixes.

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
-import express from 'express'
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+const fs = require('fs')
 
 const app = express()
 
-app.use(express.static('generic'))
+app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.options('*', cors())
+
+// app.get('/', (req, res) => {
+//   res.send('Hello world')
+// })
 
 app.listen(3000, () => {
   console.log('Express is listening to http://localhost:3000')
+})
+
+app.get('*', (req, res) => {
+  console.log('Serving request: ', req.url)
+  // res.setHeader('Cache-Control', 'public, max-age=120')
+  fs.readFile(path.resolve(__dirname, './', 'index.html'), {encoding: 'utf-8'}, (err, data) => {
+    if (err) {
+      console.log('path.resolve error: ', err)
+      res.send(500)
+    } else {
+      res.send(data)
+      // res.send(data
+      //   .replace('<title>Köp C More</title>', `<title>${pageTitle(req)}</title>`)
+      //   .replace('__CONFIG_GETS_INSERTED_HERE__', JSON.stringify(configVariables(process.env))))
+    }
+  })
 })
